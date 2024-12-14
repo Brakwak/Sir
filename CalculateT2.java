54517f48d2a75ff783404ec0815555478629eded
Brakwak <159633387+Brakwak@users.noreply.github.com>
2024-12-14 21:49:47 +0000

    Add files via upload

---
+++ CalculateT2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+	public class CalculateT2 {
+		public static void main (Strings[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter the value of a");
+			double a = sc.nextDouble();
+			
+			System.out.print("Enter the value of r");
+			double r = sc.nextDouble();
+			
+			System.out.print("Enter the value of x");
+			double x = sc.nextDouble();
+			
+			
+		};
+	};
